948bf750703b9478e68c8c9007215d75d9c0449a
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-08-07 22:56:38 +0300

    add lightbox gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -28,116 +28,167 @@ import cases62Webp from "../../images/cases/cases6@2x.webp";
 import cases6 from "../../images/cases/cases6.jpg";
 import cases62 from "../../images/cases/cases6@2x.jpg";
 
+import lightbox from "lightbox2";
+import "lightbox2/dist/css/lightbox.min.css";
+
 const Gallery = () => {
   return (
     <ul className="gallery">
       <li className="gallery__item">
-        <picture>
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+        <a
+          href={cases12}
+          data-lightbox="link"
+          data-title="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+        >
+          <picture>
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases1Webp} 1x,
                   ${cases12Webp} 2x`}
-            type="image/webp"
-          />
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+              type="image/webp"
+            />
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases1} 1x,
                     ${cases12} 2x`}
-          />
-          <img src={cases1} alt="Group of people with laptops" />
-        </picture>
+            />
+            <img
+              src={cases1}
+              alt="Group of people with laptops"
+              data-lightbox="photos"
+            />
+          </picture>
+        </a>
       </li>
       <li className="gallery__item">
-        <picture>
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+        <a
+          href={cases22}
+          data-lightbox="link"
+          data-title="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+        >
+          <picture>
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases2Webp} 1x,
                   ${cases22Webp} 2x`}
-            type="image/webp"
-          />
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+              type="image/webp"
+            />
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases2} 1x,
                     ${cases22} 2x`}
-          />
-          <img src={cases2} alt="Working place on the table" />
-        </picture>
+            />
+            <img
+              src={cases2}
+              alt="Working place on the table"
+              data-lightbox="photos"
+            />
+          </picture>
+        </a>
       </li>
       <li className="gallery__item">
-        <picture>
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+        <a
+          href={cases32}
+          data-lightbox="link"
+          data-title="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+        >
+          <picture>
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases3Webp} 1x,
                   ${cases32Webp} 2x`}
-            type="image/webp"
-          />
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+              type="image/webp"
+            />
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases3} 1x,
                     ${cases32} 2x`}
-          />
-          <img src={cases3} alt="Handshake" />
-        </picture>
+            />
+            <img src={cases3} alt="Handshake" data-lightbox="photos" />
+          </picture>
+        </a>
       </li>
       <li className="gallery__item">
-        <picture>
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+        <a
+          href={cases42}
+          data-lightbox="link"
+          data-title="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+        >
+          <picture>
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases4Webp} 1x,
                   ${cases42Webp} 2x`}
-            type="image/webp"
-          />
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+              type="image/webp"
+            />
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases4} 1x,
                     ${cases42} 2x`}
-          />
-          <img src={cases4} alt="We are team" />
-        </picture>
+            />
+            <img src={cases4} alt="We are team" data-lightbox="photos" />
+          </picture>
+        </a>
       </li>
       <li className="gallery__item">
-        <picture>
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+        <a
+          href={cases52}
+          data-lightbox="link"
+          data-title="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+        >
+          <picture>
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases5Webp} 1x,
                   ${cases52Webp} 2x`}
-            type="image/webp"
-          />
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+              type="image/webp"
+            />
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases5} 1x,
                     ${cases52} 2x`}
-          />
-          <img src={cases5} alt="Girl working" />
-        </picture>
+            />
+            <img src={cases5} alt="Girl working" data-lightbox="photos" />
+          </picture>
+        </a>
       </li>
       <li className="gallery__item">
-        <picture>
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+        <a
+          href={cases62}
+          data-lightbox="link"
+          data-title="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+        >
+          <picture>
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases6Webp} 1x,
                   ${cases62Webp} 2x`}
-            type="image/webp"
-          />
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`
+              type="image/webp"
+            />
+            <source
+              media="(min-width: 1200px)"
+              srcSet={`
                     ${cases6} 1x,
                     ${cases62} 2x`}
-          />
-          <img src={cases6} alt="A look at the sky near high-rise buildings" />
-        </picture>
+            />
+            <img
+              src={cases6}
+              alt="A look at the sky near high-rise buildings"
+              data-lightbox="photos"
+            />
+          </picture>
+        </a>
       </li>
       {/* <li className="gallery__item">
         <picture>
@@ -179,3 +230,9 @@ const Gallery = () => {
   );
 };
 export default Gallery;
+
+lightbox.option({
+  resizeDuration: 200,
+  wrapAround: true,
+  disableScrolling: true,
+});
