09b280c303536755c34cbd01d9af43ab18623e97
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-08-08 13:01:14 +0300

    edit form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -15,30 +15,33 @@ const Form = () => {
   return (
     <>
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="form__label">
-          <label>
-            <input
-              className="form__input form__name"
-              {...register("name", { required: true })}
-              placeholder="Enter your name"
-            />
-          </label>
-          <label>
-            <input
-              className="form__input form__email"
-              {...register("mail", {
-                required: "⚠  This is a required field",
-                // required: { notification },
-                // required: { warning },
-              })}
-              placeholder="Enter email*"
-            />
-          </label>
+        <div className="form__wrapper">
+          <div className="form__field">
+            <label>
+              <input
+                className="form__input form__name"
+                {...register("name", { required: true })}
+                placeholder="Enter your name"
+              />
+            </label>
+            <label>
+              <input
+                className="form__input form__email"
+                {...register("mail", {
+                  required: "⚠  This is a required field",
+                  // required: { notification },
+                  // required: { warning },
+                })}
+                placeholder="Enter email*"
+              />
+            </label>
+            <p className="form__error">{errors.mail?.message}</p>
+          </div>
+
+          <button className="button form_button" type="submit">
+            Send
+          </button>
         </div>
-        <p className="form__error">{errors.mail?.message}</p>
-        <button className="button form_button" type="submit">
-          Send
-        </button>
       </form>
     </>
   );
